10dd6faf7384c44fbe90049e09546fe1597b8dec
naimuddin94 <developernaimuddin@gmail.com>
2023-10-07 02:08:42 +0600

    banner section added

--- src/components/others/Banner.jsx
+++ src/components/others/Banner.jsx
@@ -1,5 +1,20 @@
 const Banner = () => {
-  return <div>hello world</div>;
+  return (
+    <div className="bg-[url('https://i.ibb.co/n3bcKN3/web-summit-decoded-dublin-dublinglobe-com-1066x533.jpg')] h-screen bg-cover bg-center absolute inset-0 -z-10 text-white flex items-center justify-center md:justify-start bg-black/30 bg-blend-overlay">
+      <div className="md:pl-20 lg:pl-36 px-8">
+        <h1 className="text-3xl md:text-5xl lg:text-6xl font-black py-5">
+          <span className="bg-gradient-to-t from-orange-600 to-rose-800 text-5xl md:text-6xl lg:text-8xl bg-clip-text text-transparent">
+            O
+          </span>
+          rganizer <br /> Agency
+        </h1>
+        <p className="font-mono max-w-xs">
+          We help brands and game devs reach
+          consumers through esports and video games.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
