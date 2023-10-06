3196817cf9d7943963a1f0e9abfbdb2cb1fd6c4a
naimuddin94 <developernaimuddin@gmail.com>
2023-10-06 23:27:45 +0600

    navbar create

---
+++ src/components/sharedComponents/NavLi.jsx
@@ -0,0 +1,25 @@
+import { NavLink } from "react-router-dom";
+
+const NavLi = () => {
+  return (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/services">Services</NavLink>
+      </li>
+      <li>
+        <NavLink to="/projects">Projects</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+      <li>
+        <NavLink to="/register">Register</NavLink>
+      </li>
+    </>
+  );
+};
+
+export default NavLi;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/sharedComponents/Navbar";
 
 const Layout = () => {
   return (
-    <>
+    <div className="font-inter">
+      <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 };
 
